b4b62c2b6fa7b2b7aac0521c12fddd03ab31101b
leovanj <69065411+leovanj@users.noreply.github.com>
2025-12-08 02:36:17 +0000

    fixed algebra text

--- app/math/algebra/page.jsx
+++ app/math/algebra/page.jsx
@@ -4,7 +4,7 @@
 export default function CountingPage() {
   return (
     <main>
-      <center><h2>Algebra Hub</h2></center>
+      <h2>Algebra Hub</h2>
       <p>Explore basic algebra.</p>
     </main>
   );
