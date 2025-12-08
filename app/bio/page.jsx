e02ed3f10ad2a48a1267c7ee0cae9f62a72f4736
leovanj <69065411+leovanj@users.noreply.github.com>
2025-12-08 02:26:50 +0000

    fixed bio page

---
+++ app/bio/page.jsx
@@ -0,0 +1,11 @@
+// File: app/math/page.jsx
+// Create the 'math' folder first
+
+export default function Biopage() {
+  return (
+    <main>
+      <h2> Biology</h2>
+      <p>Biology.</p>
+    </main>
+  );
+}
